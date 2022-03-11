27185ad805bdff9b2ff934eb37d52c611e9e718d
ciaracloud <ciara_cloud@yahoo.com>
2022-03-11 17:50:02 -0500

    adds index.json/creates server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejs = require("ejs");
+const { Console } = require("console");
+const PORT = 3000;
+require("dotenv").config();
+
+console.log(process.env);
+
+const imp_api_key = process.env.IMP_API_KEY;
+
+app.listen(PORT, console.log(`listening on http://localhost${PORT}`));
