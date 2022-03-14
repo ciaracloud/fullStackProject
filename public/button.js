f6d76fec95d983a4733ed9ee47c8acc5f2d5159f
JAReichert <87733614+JAReichert@users.noreply.github.com>
2022-03-14 15:38:13 -0500

    accessing the data from the db & password removed

--- public/button.js
+++ public/button.js
@@ -7,15 +7,23 @@ const links = document.querySelectorAll(".links")[0];
 
 console.log("hi");
 
+const getVacationData = async () => {
+  let url = "http://localhost:3000/db";
+  let vacationData = await fetch(url);
+  let vacationJson = await vacationData.json();
+  return vacationJson;
+};
+
 const form = document.querySelector(".form");
 const submitButton = document.querySelector(".button");
 submitButton.addEventListener("click", async (e) => {
   e.preventDefault();
+  const vacationData = await getVacationData();
   console.log("hello");
-  let vacationsData = await db.Vacations.findAll();
-  let vacationsParagraph = createElement("p");
-  vacationsParagraph.innerText = vacationsData;
+  let vacationsParagraph = document.createElement("p");
+  vacationsParagraph.innerText = vacationData;
   form.append(vacationsParagraph);
+  return vacationData;
 });
 
 // const clickMe = () => {
