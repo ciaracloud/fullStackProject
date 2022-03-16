80e5fe10aee66198a9addaeb63a47b4b9c6f5b60
JAReichert <87733614+JAReichert@users.noreply.github.com>
2022-03-15 20:33:30 -0500

    restored navbar

--- public/createVacation.js
+++ public/createVacation.js
@@ -41,3 +41,12 @@ const createVacation = async () => {
 submitButton.addEventListener("click", () => {
   createVacation();
 });
+
+//********************navbar funcitonality */
+const toggle = document.querySelectorAll(".toggle")[0];
+const links = document.querySelectorAll(".links")[0];
+
+toggle.addEventListener("click", () => {
+  links.classList.toggle("active");
+});
+//**********************end navbar functionality */
