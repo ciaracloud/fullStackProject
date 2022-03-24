e3c193d95ead1109b7b8959aa2d885815efc4c79
ciaracloud <ciara_cloud@yahoo.com>
2022-03-23 23:12:57 -0400

    adds more styling

--- public/home.js
+++ public/home.js
@@ -7,24 +7,41 @@ const checkVacationButton = document.querySelector(".checkVacationButton");
 
 const getHotelsData = async (city, vacationId) => {
   const formContainer = document.querySelector(".formContainer");
-  formContainer.remove();
   const destinationsContainer = document.querySelector(
     ".destinationsContainer"
   );
+  const thailandImage = document.querySelector(".thailandImage");
+  formContainer.remove();
   destinationsContainer.remove();
+  thailandImage.remove();
   const welcomeContainer = document.querySelector(".welcomeContainer");
-
-  const welcomeParagraph = document.createElement("p");
+  const welcomePhoto = document.createElement("img");
+  welcomePhoto.className = "welcomePhoto";
+  welcomePhoto.src =
+    "https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2235&q=80";
+  const welcomeText = document.createElement("div");
+  welcomeText.className = "welcomeText";
+  const welcomeTitle = document.createElement("p");
+  welcomeTitle.innerText = "Time to plan!";
+  welcomeTitle.className = "welcomeTitle";
+  const paragraph1 = document.createElement("p");
+  paragraph1.innerText = `Welcome ${inputsArray[0]} ${inputsArray[1]}! Let's start planning your trip to ${inputsArray[4]}!`;
+  paragraph1.className = "paragraph1";
+  const paragraph2 = document.createElement("p");
+  paragraph2.innerText = `Your reservation ID is ${vacationId.id}.`;
+  paragraph2.className = "paragraph2";
+  const paragraph3 = document.createElement("p");
+  paragraph3.className = "paragraph3";
+  paragraph3.innerText = `Make sure to keep track of it in order to look up and make changes to your trip.`;
+  const welcomeParagraph = document.createElement("div");
   welcomeParagraph.className = "welcomeParagraph";
-  welcomeParagraph.innerText = `Welcome ${inputsArray[0]} ${inputsArray[1]}!
-  Let's start planning your trip to ${inputsArray[4]}!
-  Here is your reservation ID: ${vacationId.id}.
-  Make sure to keep track of it in order to look up and make changes to your trip.`;
-  welcomeContainer.append(welcomeParagraph);
+  welcomeParagraph.append(paragraph1, paragraph2, paragraph3);
+  welcomeText.append(welcomeTitle, welcomeParagraph);
+  welcomeContainer.append(welcomePhoto, welcomeText);
 
   const hotelObject = {
     city: city,
-    url: `https://api.yelp.com/v3/businesses/search?location="${city}"&term="hotels"&limit=3`,
+    url: `https://api.yelp.com/v3/businesses/search?location="${city}"&term="hotels"&limit=4`,
     yel_api_key: `${yel_api_key}`,
   };
   let hotelInfo = await fetch("http://localhost:3000/get_hotels", {
@@ -42,36 +59,38 @@ const getHotelsData = async (city, vacationId) => {
   let hotelsJson = await hotelInfo.json();
   const hotelsContainer = document.querySelector(".hotels");
   const hotelsTitle = document.createElement("p");
-  hotelsTitle.className = "hotelsTitle";
+  hotelsTitle.className = "hotelsTitle elementTitle";
   hotelsTitle.innerText = "Choose a hotel from below:";
   hotelsContainer.append(hotelsTitle);
+  const hotelDivsContainer = document.createElement("div");
+  hotelDivsContainer.className = "hotelDivsContainer divContainer";
   let i = 0;
   for (const hotel of hotelsJson) {
     i++;
     const hotelDiv = document.createElement("div");
-    hotelDiv.className = `hotelDiv${i}`;
+    hotelDiv.className = `hotelDiv${i} hotelDiv elementDiv`;
     const hotelName = document.createElement("p");
     hotelName.innerText = hotel.name;
-    hotelName.className = "hotelName";
+    hotelName.className = "hotelName name";
     const hotelImg = document.createElement("img");
     hotelImg.src = hotel.image_url;
     hotelImg.height = "200";
-    hotelImg.className = "hotelImg";
+    hotelImg.className = "hotelImg img";
     const hotelRating = document.createElement("p");
     hotelRating.innerText = `Rating: ${hotel.rating}`;
-    hotelRating.className = "hotelRating";
+    hotelRating.className = "hotelRating rating";
     const hotelPrice = document.createElement("p");
     hotelPrice.innerText = hotel.price;
-    hotelPrice.className = "hotelPrice";
+    hotelPrice.className = "hotelPrice price";
     const hotelAddress = document.createElement("p");
     hotelAddress.innerText = hotel.location.display_address;
-    hotelAddress.className = "hotelAddress";
+    hotelAddress.className = "hotelAddress address";
     const hotelPhone = document.createElement("p");
     hotelPhone.innerText = hotel.display_phone;
-    hotelPhone.className = "hotelPhone";
+    hotelPhone.className = "hotelPhone phone";
     const hotelAddButton = document.createElement("button");
     hotelAddButton.innerText = "Add";
-    hotelAddButton.className = "hotelAddButton";
+    hotelAddButton.className = "hotelAddButton addButton";
 
     const addHotelToDB = async () => {
       hotelsContainer.remove();
@@ -114,8 +133,9 @@ const getHotelsData = async (city, vacationId) => {
       hotelPhone,
       hotelAddButton
     );
-    hotelsContainer.append(hotelDiv);
+    hotelDivsContainer.append(hotelDiv);
   }
+  hotelsContainer.append(hotelDivsContainer);
   const restaurantsButton = document.createElement("button");
   restaurantsButton.innerText = "See restaurants";
   restaurantsButton.className = "restaurantButton";
@@ -124,7 +144,7 @@ const getHotelsData = async (city, vacationId) => {
   const getRestaurantsData = async () => {
     const restaurantObject = {
       city: city,
-      url: `https://api.yelp.com/v3/businesses/search?location="${city}"&term="restaurants"&limit=3`,
+      url: `https://api.yelp.com/v3/businesses/search?location="${city}"&term="restaurants"&limit=4`,
       yel_api_key: `${yel_api_key}`,
     };
     let restaurantInfo = await fetch("http://localhost:3000/get_restaurants", {
@@ -142,36 +162,39 @@ const getHotelsData = async (city, vacationId) => {
     let restaurantJson = await restaurantInfo.json();
     const restaurantContainer = document.querySelector(".restaurants");
     const restaurantTitle = document.createElement("p");
-    restaurantTitle.className = "restaurantTitle";
+    restaurantTitle.className = "restaurantTitle elementTitle";
     restaurantTitle.innerText = "Choose restaurant(s) from below:";
     restaurantContainer.append(restaurantTitle);
+    const restaurantsDivsContainer = document.createElement("div");
+    restaurantsDivsContainer.className =
+      "restaurantsDivsContainer divContainer";
     let i = 0;
     for (const restaurant of restaurantJson) {
       i++;
       const restaurantDiv = document.createElement("div");
-      restaurantDiv.className = `restaurantDiv${i}`;
+      restaurantDiv.className = `restaurantDiv${i} restaurantDiv elementDiv`;
       const restaurantName = document.createElement("p");
       restaurantName.innerText = restaurant.name;
-      restaurantName.className = "restaurantName";
+      restaurantName.className = "restaurantName name";
       const restaurantImg = document.createElement("img");
       restaurantImg.src = restaurant.image_url;
       restaurantImg.height = "200";
-      restaurantImg.className = "restaurantImg";
+      restaurantImg.className = "restaurantImg img";
       const restaurantPrice = document.createElement("p");
       restaurantPrice.innerText = restaurant.price;
-      restaurantPrice.className = "restaurantPrice";
+      restaurantPrice.className = "restaurantPrice price";
       const restaurantRating = document.createElement("p");
       restaurantRating.innerText = `Rating: ${restaurant.rating}`;
-      restaurantRating.className = "restaurantRating";
+      restaurantRating.className = "restaurantRating rating";
       const restaurantAddress = document.createElement("p");
       restaurantAddress.innerText = restaurant.location.display_address;
-      restaurantAddress.className = "restaurantAddress";
+      restaurantAddress.className = "restaurantAddress address";
       const restaurantPhone = document.createElement("p");
       restaurantPhone.innerText = restaurant.display_phone;
-      restaurantPhone.className = "restaurantPhone";
+      restaurantPhone.className = "restaurantPhone phone";
       const restaurantAddButton = document.createElement("button");
       restaurantAddButton.innerText = "Add";
-      restaurantAddButton.className = "restaurantAddButton";
+      restaurantAddButton.className = "restaurantAddButton addButton";
 
       const addRestaurantToDB = async () => {
         restaurantDiv.remove();
@@ -218,9 +241,9 @@ const getHotelsData = async (city, vacationId) => {
         restaurantPhone,
         restaurantAddButton
       );
-
-      restaurantContainer.append(restaurantDiv);
+      restaurantsDivsContainer.append(restaurantDiv);
     }
+    restaurantContainer.append(restaurantsDivsContainer);
     const excursionsButton = document.createElement("button");
     excursionsButton.innerText = "See excursions";
     excursionsButton.className = "excursionsButton";
@@ -229,7 +252,7 @@ const getHotelsData = async (city, vacationId) => {
     const getExcursionsData = async () => {
       const excursionObject = {
         city: city,
-        url: `https://api.yelp.com/v3/businesses/search?location="${city}"&term="excursions"&limit=3`,
+        url: `https://api.yelp.com/v3/businesses/search?location="${city}"&term="excursions"&limit=4`,
         yel_api_key: `${yel_api_key}`,
       };
       let excursionInfo = await fetch("http://localhost:3000/get_excursions", {
@@ -247,30 +270,33 @@ const getHotelsData = async (city, vacationId) => {
       let excursionsJson = await excursionInfo.json();
       const excursionsContainer = document.querySelector(".excursions");
       const excursionTitle = document.createElement("p");
-      excursionTitle.className = "excursionTitle";
+      excursionTitle.className = "excursionTitle elementTitle";
       excursionTitle.innerText = "Choose an excursion(s) from below:";
       excursionsContainer.append(excursionTitle);
+      const excursionsDivsContainer = document.createElement("div");
+      excursionsDivsContainer.className =
+        "excursionsDivsContainer divContainer";
       let i = 0;
       for (const excursion of excursionsJson) {
         i++;
         const excursionDiv = document.createElement("div");
-        excursionDiv.className = `excursionDiv${i}`;
+        excursionDiv.className = `excursionDiv${i} excursionDiv elementDiv`;
         const excursionName = document.createElement("p");
         excursionName.innerText = excursion.name;
-        excursionName.className = "excursionName";
+        excursionName.className = "excursionName name";
         const excursionImg = document.createElement("img");
         excursionImg.src = excursion.image_url;
         excursionImg.height = "200";
-        excursionImg.className = "excursionImg";
+        excursionImg.className = "excursionImg img";
         const excursionRating = document.createElement("p");
         excursionRating.innerText = `Rating: ${excursion.rating}`;
-        excursionRating.className = "excursionRating";
+        excursionRating.className = "excursionRating rating";
         const excursionAddress = document.createElement("p");
         excursionAddress.innerText = excursion.location.display_address;
-        excursionAddress.className = "excursionAddress";
+        excursionAddress.className = "excursionAddress address";
         const excursionPhone = document.createElement("p");
         excursionPhone.innerText = excursion.display_phone;
-        excursionPhone.className = "excursionPhone";
+        excursionPhone.className = "excursionPhone phone";
         const excursionAddButton = document.createElement("button");
         excursionAddButton.innerText = "Add";
         excursionAddButton.className = "excursionAddButton";
@@ -317,8 +343,9 @@ const getHotelsData = async (city, vacationId) => {
           excursionPhone,
           excursionAddButton
         );
-        excursionsContainer.append(excursionDiv);
+        excursionsDivsContainer.append(excursionDiv);
       }
+      excursionsContainer.append(excursionsDivsContainer);
       const finalPageButton = document.createElement("button");
       finalPageButton.className = "finalPageButton";
       finalPageButton.innerText = "See vacation";
